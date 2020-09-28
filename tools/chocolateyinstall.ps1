90b0f383295ec468d0ab9e2f60e515cd066132f9
KryptoKoder Support <support@kryptokoder.com>
2020-09-27 18:19:45 -0700

    addressed comments from reviewer

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,20 +2,17 @@
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://www.kryptokoder.com/MyPDFSigner-3.1.1-1-x86_64.exe'
-$url64      = ''
+$url64      = 'https://www.kryptokoder.com/MyPDFSigner-3.1.1-1-x86_64.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
-  url           = $url
   url64bit      = $url64
 
   softwareName  = 'mypdfsigner*'
 
-  checksum      = '0ae23cb05bffe78d26a0b8bd9a285c4c820e30285c1865db536a7922cfc758f4'
-  checksumType  = 'sha256'
-  checksum64    = ''
+  checksum64    = '0ae23cb05bffe78d26a0b8bd9a285c4c820e30285c1865db536a7922cfc758f4'
   checksumType64= 'sha256'
 
   validExitCodes= @(0, 3010, 1641)
@@ -23,22 +20,3 @@ $packageArgs = @{
 }
 
 Install-ChocolateyPackage @packageArgs
-
-
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
-
